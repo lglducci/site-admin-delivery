73fa10d4b549b3321cbf63c5ec3f04a0e1ce4d44
lglducci <lglducci@hotmail.com>
2025-09-06 11:07:48 -0300

    novo projeto

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,10 @@
+
+function Dashboard() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-green-100">
+      <h2 className="text-3xl font-bold">Bem-vindo ao Painel Admin Delivery</h2>
+    </div>
+  );
+}
+
+export default Dashboard;
