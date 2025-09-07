4581023112ab0f1f5d8619c4b5c6a5b8645436df
lglducci <lglducci@hotmail.com>
2025-09-07 11:18:08 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  darkMode: 'class',
+ 
+
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
