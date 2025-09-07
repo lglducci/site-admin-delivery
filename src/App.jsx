3c32d05608c53b70ad60ced7aa4ab14a82e0b9cb
lglducci <lglducci@hotmail.com>
2025-09-07 11:12:02 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import React from "react";
-import Login from "./pages/Login";
+ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
 
-export default function App() {
-  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
-  return isLoggedIn ? <Dashboard /> : <Login onLogin={() => setIsLoggedIn(true)} />;
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
+export default App;
