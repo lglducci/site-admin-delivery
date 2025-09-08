d5f80bcc2673164ce484cb3d305bcc8d1d99ccfb
lglducci <lglducci@hotmail.com>
2025-09-08 18:21:17 -0300

    Update PedidoCard.jsx

--- src/components/PedidoCard.jsx
+++ src/components/PedidoCard.jsx
@@ -10,7 +10,7 @@ export default function PedidoCard({ pedido }) {
     minute: "2-digit",
   });
 
-  const mostrarData = ehHoje ? "hoje" : dataPedido.toLocaleDateString("pt-BR");
+ /// const mostrarData = ehHoje ? "hoje" : dataPedido.toLocaleDateString("pt-BR");
 
  const handleAvancar = async () => {
     try {
@@ -32,6 +32,26 @@ export default function PedidoCard({ pedido }) {
     window.location.reload();
   };
 
+const cancelarPedido = async (numero) => {
+  try {
+    const resposta = await fetch(`https://webhook.lglducci.com.br/webhook/cancelar`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ numero }),
+    });
+
+    if (!resposta.ok) throw new Error("Erro ao cancelar pedido");
+    alert(`Pedido nº ${numero} cancelado com sucesso.`);
+    window.location.reload();
+  } catch (erro) {
+    alert("Erro ao cancelar o pedido.");
+  }
+};
+
+
+
+ 
+
   return (
     <div className="flex justify-between items-center px-3 py-2 rounded bg-gray-100 dark:bg-gray-700 mb-2 text-sm font-medium text-gray-800 dark:text-white">
       <div className="flex-1">
@@ -48,18 +68,36 @@ export default function PedidoCard({ pedido }) {
         </span>
       </div>
 
-      <div className="text-gray-500 dark:text-gray-300 text-xs mx-2">
-        {mostrarData} às {hora}
-      </div>
+      //<div className="text-gray-500 dark:text-gray-300 text-xs mx-2">
+     //   {mostrarData} às {hora}
+     // </div>
 
-      <button
-        onClick={handleAvancar}
-        className="text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-white text-xs"
-      >
-        Avançar
-      </button>
+   <div className="text-gray-500 dark:text-gray-300 text-xs mx-2">
+      {hora}
+    </div>
+
+<div className="flex space-x-2">
+  <span className="text-blue-600 dark:text-blue-300 text-sm">📦</span>
+  <button
+    onClick={handleAvancar}
+    className="text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-white text-xs"
+  >
+    Avançar
+  </button>
+  <button
+    onClick={() => cancelarPedido(pedido.numero)}
+    className="text-red-600 hover:text-red-800 dark:text-red-400 dark:hover:text-white text-xs"
+  >
+    Cancelar
+  </button>
+</div>
+
+ 
+     
+      
     </div>
   );
 }
+
 
 
