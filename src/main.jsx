b2864371faf7b062c637ecd030773dff8c395484
lglducci <lglducci@hotmail.com>
2025-10-04 12:47:10 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { EmpresaProvider } from "./context/EmpresaContext.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <EmpresaProvider>
+      <App />
+    </EmpresaProvider>
+  </React.StrictMode>
+);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
