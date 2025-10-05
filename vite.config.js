ab725b2e25a62f586ecbca5cc71fd9f72a1090b3
lglducci <lglducci@hotmail.com>
2025-10-04 21:21:24 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
-import { defineConfig } from 'vite'
+ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api/webhook': {
+        target: 'https://webhook.lglducci.com.br',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/webhook/, '/webhook'),
+      },
+    },
+  },
 })
