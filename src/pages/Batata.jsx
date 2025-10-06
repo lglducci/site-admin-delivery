2faff118e7bfba1bb159d58fcabb88f217abec47
lglducci <lglducci@hotmail.com>
2025-10-06 14:26:09 -0300

    Create Batata.jsx

---
+++ src/pages/Batata.jsx
@@ -0,0 +1,18 @@
+// src/pages/Batata.jsx
+export default function Batata() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "#000",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <span style={{ color: "#FF1493", fontSize: 48, fontWeight: 800 }}>
+        hello world
+      </span>
+    </div>
+  );
+}
