3b8e8943bf028a26542fea051b6698129054f4b3
lglducci <lglducci@hotmail.com>
2025-10-06 17:43:33 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,14 +11,14 @@ export default defineConfig({
         target: 'https://webhook.lglducci.com.br/webhook',
         changeOrigin: true,
         secure: false,
-        rewrite: (p) => p.replace(/^\/api\/webhook/, ''), // -> /modelos_custo...
+        rewrite: (p) => p.replace(/^\/api\/webhook/, ''), // -> /modelo...
       },
       // Teste (n8n webhook-test)
       '/api/n8n': {
         target: 'https://n8n.lglducci.com.br/webhook-test',
         changeOrigin: true,
         secure: false,
-        rewrite: (p) => p.replace(/^\/api\/n8n/, ''), // -> /modelos_custo...
+        rewrite: (p) => p.replace(/^\/api\/n8n/, ''), // -> /modelo...
       },
     },
   },
