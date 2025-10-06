187ae531828d1eb57c048f9938563c706d9981a7
lglducci <lglducci@hotmail.com>
2025-10-06 11:46:51 -0300

    Create ModelosCustoPage.jsx

---
+++ src/pages/ModelosCustoPage.jsx
@@ -0,0 +1,20 @@
+export default function ModelosCustoPage() {
+  console.log("🟢 ModelosCustoPage montou!");
+  alert("🟢 ModelosCustoPage montou!"); // prova visual
+
+  return (
+    <div
+      style={{
+        backgroundColor: "black",
+        color: "lime",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "28px",
+      }}
+    >
+      🚀 ModelosCustoPage carregou!
+    </div>
+  );
+}
