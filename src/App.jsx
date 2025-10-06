94798d45ae08d39b4b34b13019dea19e46075694
lglducci <lglducci@hotmail.com>
2025-10-06 18:53:28 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Pedidos from "./pages/Pedidos";
 import Cardapio from "./pages/Cardapio";
 import EditarItem from "./pages/EditarItem";
 import PedidoDetalhes from "./pages/PedidoDetalhes";
-  
+import PizzaModelo from "./pages/PizzaModelo.jsx"; // + import
      
 
 export default function App() {
@@ -19,7 +19,7 @@ export default function App() {
         <Route path="/cardapio" element={<Cardapio />} />
         <Route path="/editar-item/:id" element={<EditarItem />} />
         <Route path="/detalhes" element={<PedidoDetalhes />} />
-      
+       <Route path="/pizza-modelo" element={<PizzaModelo />} />
       </Routes>
     </Router>
   );
