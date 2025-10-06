9df06a3533048ec72bc23b8d0b9fac69e2514993
lglducci <lglducci@hotmail.com>
2025-10-06 18:18:20 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Pedidos from "./pages/Pedidos";
 import Cardapio from "./pages/Cardapio";
 import EditarItem from "./pages/EditarItem";
 import PedidoDetalhes from "./pages/PedidoDetalhes";
-import Modelo from "./pages/modelo.jsx"; // <- igual ao nome do arquivo (minúsculo)
+import Modelo from "./pages/Modelo.jsx"; // <- igual ao nome do arquivo (minúsculo)
 
 export default function App() {
   return (
