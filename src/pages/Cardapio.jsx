083b5c83f12bc3a57135d0c5ca4c3aeb1406d68b
lglducci <lglducci@hotmail.com>
2025-10-10 21:56:51 -0300

    Update Cardapio.jsx

--- src/pages/Cardapio.jsx
+++ src/pages/Cardapio.jsx
@@ -67,7 +67,9 @@ export default function Cardapio() {
         </div>
       </div>
 {/* Cards */}
-<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-6">
+{/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-6"> */}
+ <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-7 gap-6">
+
 
   {itensFiltrados.map((item) => (
   <div
