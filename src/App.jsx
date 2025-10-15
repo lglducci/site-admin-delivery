a4ea4364709ef29d740a370a209c2ed841ed6557
lglducci <lglducci@hotmail.com>
2025-10-15 16:45:46 -0300

    Refactor routes and imports in App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,30 @@ import Pedidos from "./pages/Pedidos";
 import Cardapio from "./pages/Cardapio";
 import EditarItem from "./pages/EditarItem";
 import PedidoDetalhes from "./pages/PedidoDetalhes";
-import PizzaModelo from "./pages/PizzaModelo.jsx"; // + import
+import PizzaModelo from "./pages/PizzaModelo.jsx";
 import Relatorios from "./pages/Relatorios.jsx";
- import KpiTermometroDia from "./pages/KpiTermometroDia";
-import KdsView from "./pages/KdsView"; // adicione o import
+import KpiTermometroDia from "./pages/KpiTermometroDia";
+import KdsView from "./pages/KdsView";
 
 export default function App() {
   return (
     <Router>
       <Routes>
+        {/* Todas as rotas que compartilham o Layout */}
         <Route element={<Layout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/pedidos" element={<Pedidos />} />
+          <Route path="/cardapio" element={<Cardapio />} />
+          <Route path="/editar-item/:id" element={<EditarItem />} />
+          <Route path="/detalhes" element={<PedidoDetalhes />} />
+          <Route path="/pizza-modelo" element={<PizzaModelo />} />
+          <Route path="/relatorios" element={<Relatorios />} />
+          <Route path="/kpi" element={<KpiTermometroDia />} />
+          <Route path="/kds" element={<KdsView />} />
+        </Route>
+
+        {/* Login fora do layout (sem menu) */}
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/pedidos" element={<Pedidos />} />
-        <Route path="/cardapio" element={<Cardapio />} />
-        <Route path="/editar-item/:id" element={<EditarItem />} />
-        <Route path="/detalhes" element={<PedidoDetalhes />} />
-       <Route path="/pizza-modelo" element={<PizzaModelo />} />
-       <Route path="/relatorios" element={<Relatorios />} />
-       <Route path="/kpi" element={<KpiTermometroDia />} />
-       <Route path="/kds" element={<KdsView />} />
       </Routes>
     </Router>
   );
