6ef7f169ffc3981209319cdfc9955c87d8d815e4
lglducci <lglducci@hotmail.com>
2025-10-17 10:22:00 -0300

    Update Layout.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -14,7 +14,15 @@ export default function Layout() {
     <div className="min-h-screen bg-[#0F0F0F] text-white flex flex-col">
       {/* Menu fixo */}  
       <header className="bg-[#0c1d2a] border-b border-[#ff9f43]/30 px-4 py-3 flex justify-between items-center">
-        <div className="font-bold text-[#ff9f43] text-lg">🍕 AdminDelivery</div>
+
+<div
+  className="font-extrabold tracking-tight text-2xl md:text-3xl"
+  style={{ color: "#ff9f43" }}
+>
+  🍕 AdminDelivery
+</div>
+       
+    
 
         {/* Se for /kds, mostra só o botão sair */}
         {isKdsView ? (
